a94cb53e486cd730b8123a9ab1b75d0fa2ebfa5d
Jaen <jaennova9@gmail.com>
2023-02-13 19:40:02 -0600

    tema nuevo agregado

---
+++ bspwm/themes/Tema/theme.bash
@@ -0,0 +1,78 @@
+
+# Colors
+background='#44364d'
+foreground='#fffcab'
+
+color0='#c0c5ce'
+color1='#97b5b4'
+color2='#b38ea7'
+color3='#90a1b2'
+color4='#e9ca8e'
+color5='#bd616b'
+color6='#99bd8e'
+color7='#2b303b'
+color8='#d1d6e1'
+color9='#b0c9c9'
+color10='#c8b2b8'
+color11='#a8b3c3'
+color12='#f9e3a9'
+color13='#bd616b'
+color14='#b3d1a9'
+color15='#3c3f4d'
+
+accent='#f9e3a9'
+light_value='0.05'
+dark_value='0.30'
+
+# Wallpaper
+wdir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+wallpaper="/home/jaen/.config/bspwm/themes/des.jpg"
+
+# Polybar
+polybar_font='Iosevka Nerd Font:size=10;3'
+
+# Rofi
+rofi_font='Iosevka 10'
+rofi_icon='Papirus-Apps'
+
+# Terminal
+terminal_font_name='JetBrainsMono Nerd Font'
+terminal_font_size='10'
+
+# Geany
+geany_colors='adapta.conf'
+geany_font='JetBrains Mono 10'
+
+# Appearance
+gtk_font='Noto Sans 9'
+gtk_theme='Lightningbug-Dark'
+icon_theme='WhiteSur-grey-dark'
+cursor_theme='McMojave Cursors'
+
+# Dunst
+dunst_width='300'
+dunst_height='80'
+dunst_offset='10x40'
+dunst_origin='top-right'
+dunst_font='Iosevka Custom 9'
+dunst_border='2'
+dunst_separator='2'
+
+# Picom
+picom_backend='glx'
+picom_corner='0'
+picom_shadow_r='14'
+picom_shadow_o='0.30'
+picom_shadow_x='-12'
+picom_shadow_y='-12'
+picom_blur_method='none'
+picom_blur_strength='0'
+
+# Bspwm
+bspwm_fbc="$accent"
+bspwm_nbc="$background"
+bspwm_abc="$color5"
+bspwm_pfc="$color2"
+bspwm_border='2'
+bspwm_gap='10'
+bspwm_sratio='0.50'
